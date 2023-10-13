a3bcb67e454b0005918e8589fce33ae6985312d9
Nikki Bakshi <nikki.bakshi@correlation-one.com>
2023-10-13 20:26:58 +0530

    added code for schedule call

--- src/components/ScheduleCall/schedule.js
+++ src/components/ScheduleCall/schedule.js
@@ -1,59 +1,50 @@
-import './ScheduleCall.css'
-import Calendar from 'react-calendar';
+import "./ScheduleCall.css";
+import Calendar from "react-calendar";
 import { useState, useEffect } from "react";
 import "../../assets/css/style.css";
 import "../../assets/css/responsive.css";
 import "../../assets/css/bootstrape.css";
-import profileImage from "../../assets/images/dharmendra.png";
-import 'react-calendar/dist/Calendar.css';
-import formConfig from "../../formconfig.json";
+import "react-calendar/dist/Calendar.css";
 import { posssibleSlots } from "./slotGenerator";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import axios from 'axios';
-import img from '../../assets/images/dummy.jpg'
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-const datesD = []
+import axios from "axios";
+import img from "../../assets/images/dummy.jpg";
+// const monthNames = ["January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"
+// ];
+const datesD = [];
 function Schedule(props) {
+  const [slectedSlot, setSelectedSlot] = useState(null);
   const [slots, setSlots] = useState([]);
-  const [date, setDate] = useState(new Date());
   const [slot, setSlot] = useState(null);
   const [value, onChange] = useState(new Date());
   const [apiData, setApiData] = useState(null);
-  const [disabledDates, setDisabledDates] = useState([new Date()]);
-
-  const handleNavigate = () => {
-
-    const nextMonth = new Date(value);
-    nextMonth.setMonth(value.getMonth() + 1);
-    onChange(nextMonth);
-
-  };
+  const [timeZone, setTimeZone] = useState(null);
   useEffect(() => {
-
+    const currentTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    setTimeZone(currentTimeZone);
     async function fetchData() {
       try {
-        const data = await axios.get('http://api.flickerp.com/api/v1/marketing/event-configs/6/');
+        const data = await axios.get(process.env.REACT_APP_URL);
         setApiData(data?.data);
       } catch (e) {
         console.log(e);
       }
     }
-
     fetchData();
   }, []);
   useEffect(() => {
     if (apiData && value) {
       const { date_range } = apiData;
-      const formattedDate = `${value.getFullYear()}-${value.getMonth() + 1}-${value.getDate()}`;
+      const formattedDate = `${value.getFullYear()}-${
+        value.getMonth() + 1
+      }-${value.getDate()}`;
       const selectedDate = date_range[formattedDate];
       if (selectedDate) {
         const { start, end } = selectedDate;
         if (start && end) {
           setSlots(
             posssibleSlots({
-              time_duration: '30 min',
+              time_duration: "30 min",
               start_time: start,
               end_time: end,
             })
@@ -65,46 +56,45 @@ function Schedule(props) {
     }
   }, [apiData, value]);
   function nextHandler() {
-    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     let dataObject = undefined;
     if (slots.length > 0)
-      dataObject = { slot: (!slot ? `${slots[0].starts + '-' + slots[0].ends}` : slot), date, timeZone }
-    if (!dataObject) alert('Slot is not selected');
+      dataObject = {
+        slot: !slot ? `${slots[0].starts + "-" + slots[0].ends}` : slot,
+        value,
+        timeZone,
+      };
+    if (!dataObject) alert("Slot is not selected");
     else handleClick(dataObject);
   }
-  function getTodayDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-
   const handleClick = (dataObject) => {
     props.changeState("info");
     props.setScheduleData(dataObject);
   };
   useEffect(() => {
-
     if (apiData !== null) {
-
       for (let x in apiData?.date_range) {
-        setDisabledDates(prev => [...prev, new Date(x)]);
-        datesD.push(new Date(x))
+        datesD.push(new Date(x));
       }
     }
-  }, [apiData])
+  }, [apiData]);
   const isDateDisabled = (date) => {
-
     const disabledDates = datesD;
-    return !disabledDates.some(disabledDate => date.getDate() === disabledDate.getDate() && date.getMonth() === disabledDate.getMonth() && date.getFullYear() === disabledDate.getFullYear());
+    return !disabledDates.some(
+      (disabledDate) =>
+        date.getDate() === disabledDate.getDate() &&
+        date.getMonth() === disabledDate.getMonth() &&
+        date.getFullYear() === disabledDate.getFullYear()
+    );
   };
   return (
     <div className="row g-0">
       <div className="col-md-4">
         <div className="wizard_right_content">
           <div className="wizard_right_detail text-center">
-            <img src={(apiData?.employee?.img) ? apiData?.employee?.img : img} alt="profile"></img>
+            <img
+              src={apiData?.employee?.img ? apiData?.employee?.img : img}
+              alt="profile"
+            ></img>
             <h4>{`${apiData?.employee?.user?.firstname} ${apiData?.employee?.user?.lastname}`}</h4>
             <span>{apiData?.employee?.designation?.name}</span>
           </div>
@@ -119,151 +109,159 @@ function Schedule(props) {
                 <div className="col-md-7">
                   <div className="date">
                     <label for="date">Date*</label>
-                    <br />
-                    <div className='custom-navigation'>
-                      <div className='current-date'>
-                        <h3>{monthNames[value.getMonth()]} {value.getFullYear()}</h3>
-                      </div>
-                      <div className='next-navigate-button'>
-                        <ArrowForwardIcon onClick={handleNavigate} />
-                      </div>
-                    </div>
+                    <br />{" "}
                     <Calendar
                       onChange={onChange}
                       month={value}
                       allowPartialRange={true}
                       defaultActiveStartDate={new Date()}
                       value={value}
-                      view='month'
+                      view="month"
                       showNeighboringMonth={false}
                       showNavigation={true}
                       tileDisabled={({ date }) => isDateDisabled(date)}
-                    />
-
+                    />{" "}
                   </div>
                 </div>
                 <div className="col-md-5">
                   <div className="time">
-                    <label for="time">Time*</label>
-
-                    <br />
-
-                    {slots.length > 0 ? <div className='slot-container'>
-
-                      {slots.map((data, i) => {
-                        return (
-                          <div
-                            className='slot' key={i}
-                            onClick={() => {
-                              setSlot(`${data.starts} - ${data.ends}`)
-                            }}
-                          >
-                            <p >
-                              {data.starts} - {data.ends}
-                            </p>
-                          </div>
-                        );
-                      })}
-                    </div> : <div style={{
-                      color: 'red',
-                      fontFamily: 'sans-serif',
-                      fontWeight: 600,
-                      textSize: '30px'
-                    }}>
-                      <p>No Slots availabel for this date</p>
-                    </div>}
+                    <label for="time">Time*</label> <br />{" "}
+                    {slots.length > 0 ? (
+                      <div className="slot-container">
+                        {" "}
+                        {slots.map((data, i) => {
+                          return (
+                            <div
+                              className={
+                                slectedSlot === i
+                                  ? "slot selected-slot"
+                                  : "slot"
+                              }
+                              key={i}
+                              onClick={() => {
+                                setSelectedSlot(i);
+                                setSlot(`${data.starts} - ${data.ends}`);
+                              }}
+                            >
+                              <p>
+                                {data.starts} - {data.ends}
+                              </p>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    ) : (
+                      <div
+                        style={{
+                          color: "red",
+                          fontFamily: "sans-serif",
+                          fontWeight: 600,
+                          textSize: "30px",
+                        }}
+                      >
+                        <p>No Slots availabel for this date</p>
+                      </div>
+                    )}
                   </div>
                 </div>
                 <div className="col-md-12">
                   <div className="timezone">
                     <label for="timezone">Timezone*</label>{" "}
-                    <select name="timezone">
-                      <option value="-12">
-                        (GMT -12:00) Eniwetok, Kwajalein
-                      </option>
-                      <option value="-11">
-                        (GMT -11:00) Midway Island, Samoa
-                      </option>
-                      <option value="-10">(GMT -10:00) Hawaii</option>
-                      <option value="-9">(GMT -9:00) Alaska</option>
-                      <option value="-8">
-                        (GMT -8:00) Pacific Time (US &amp; Canada)
-                      </option>
-                      <option value="-7">
-                        (GMT -7:00) Mountain Time (US &amp; Canada)
-                      </option>
-                      <option value="-6">
-                        (GMT -6:00) Central Time (US &amp; Canada), Mexico City
-                      </option>
-                      <option value="-5">
-                        (GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima
-                      </option>
-                      <option value="-4.5">(GMT -4:30) Caracas</option>
-                      <option value="-4">
-                        (GMT -4:00) Atlantic Time (Canada), La Paz, Santiago
-                      </option>
-                      <option value="-3.5">(GMT -3:30) Newfoundland</option>
-                      <option value="-3">
-                        (GMT -3:00) Brazil, Buenos Aires, Georgetown
-                      </option>
-                      <option value="-2">(GMT -2:00) Mid-Atlantic</option>
-                      <option value="-1">
-                        (GMT -1:00 hour) Azores, Cape Verde Islands
-                      </option>
-                      <option selected="selected" value="0">
-                        (GMT) Western Europe Time, London, Lisbon, Casablanca,
-                        Greenwich
-                      </option>
-                      <option value="1">
-                        (GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris
-                      </option>
-                      <option value="2">
-                        (GMT +2:00) Kaliningrad, South Africa, Cairo
-                      </option>
-                      <option value="3">
-                        (GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg
-                      </option>
-                      <option value="3.5">(GMT +3:30) Tehran</option>
-                      <option value="4">
-                        (GMT +4:00) Abu Dhabi, Muscat, Yerevan, Baku, Tbilisi
-                      </option>
-                      <option value="4.5">(GMT +4:30) Kabul</option>
-                      <option value="5">
-                        (GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent
-                      </option>
-                      <option value="5.5">
-                        (GMT +5:30) Mumbai, Kolkata, Chennai, New Delhi
-                      </option>
-                      <option value="5.75">(GMT +5:45) Kathmandu</option>
-                      <option value="6">
-                        (GMT +6:00) Almaty, Dhaka, Colombo
-                      </option>
-                      <option value="6.5">
-                        (GMT +6:30) Yangon, Cocos Islands
-                      </option>
-                      <option value="7">
-                        (GMT +7:00) Bangkok, Hanoi, Jakarta
-                      </option>
-                      <option value="8">
-                        (GMT +8:00) Beijing, Perth, Singapore, Hong Kong
-                      </option>
-                      <option value="9">
-                        (GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk
-                      </option>
-                      <option value="9.5">(GMT +9:30) Adelaide, Darwin</option>
-                      <option value="10">
-                        (GMT +10:00) Eastern Australia, Guam, Vladivostok
-                      </option>
-                      <option value="11">
-                        (GMT +11:00) Magadan, Solomon Islands, New Caledonia
-                      </option>
-                      <option value="12">
-                        (GMT +12:00) Auckland, Wellington, Fiji, Kamchatka
-                      </option>
-                    </select>
+                    {timeZone && (
+                      <select name="timezone">
+                        <option selected value="-13">
+                          {timeZone}
+                        </option>
+                        <option value="-12">
+                          (GMT -12:00) Eniwetok, Kwajalein
+                        </option>
+                        <option value="-11">
+                          (GMT -11:00) Midway Island, Samoa
+                        </option>
+                        <option value="-10">(GMT -10:00) Hawaii</option>
+                        <option value="-9">(GMT -9:00) Alaska</option>
+                        <option value="-8">
+                          (GMT -8:00) Pacific Time (US &amp; Canada)
+                        </option>
+                        <option value="-7">
+                          (GMT -7:00) Mountain Time (US &amp; Canada)
+                        </option>
+                        <option value="-6">
+                          (GMT -6:00) Central Time (US &amp; Canada), Mexico
+                          City
+                        </option>
+                        <option value="-5">
+                          (GMT -5:00) Eastern Time (US &amp; Canada), Bogota,
+                          Lima
+                        </option>
+                        <option value="-4.5">(GMT -4:30) Caracas</option>
+                        <option value="-4">
+                          (GMT -4:00) Atlantic Time (Canada), La Paz, Santiago
+                        </option>
+                        <option value="-3.5">(GMT -3:30) Newfoundland</option>
+                        <option value="-3">
+                          (GMT -3:00) Brazil, Buenos Aires, Georgetown
+                        </option>
+                        <option value="-2">(GMT -2:00) Mid-Atlantic</option>
+                        <option value="-1">
+                          (GMT -1:00 hour) Azores, Cape Verde Islands
+                        </option>
+                        <option value="0">
+                          (GMT) Western Europe Time, London, Lisbon, Casablanca,
+                          Greenwich
+                        </option>
+                        <option value="1">
+                          (GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris
+                        </option>
+                        <option value="2">
+                          (GMT +2:00) Kaliningrad, South Africa, Cairo
+                        </option>
+                        <option value="3">
+                          (GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg
+                        </option>
+                        <option value="3.5">(GMT +3:30) Tehran</option>
+                        <option value="4">
+                          (GMT +4:00) Abu Dhabi, Muscat, Yerevan, Baku, Tbilisi
+                        </option>
+                        <option value="4.5">(GMT +4:30) Kabul</option>
+                        <option value="5">
+                          (GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent
+                        </option>
+                        <option value="5.5">
+                          (GMT +5:30) Mumbai, Kolkata, Chennai, New Delhi
+                        </option>
+                        <option value="5.75">(GMT +5:45) Kathmandu</option>
+                        <option value="6">
+                          (GMT +6:00) Almaty, Dhaka, Colombo
+                        </option>
+                        <option value="6.5">
+                          (GMT +6:30) Yangon, Cocos Islands
+                        </option>
+                        <option value="7">
+                          (GMT +7:00) Bangkok, Hanoi, Jakarta
+                        </option>
+                        <option value="8">
+                          (GMT +8:00) Beijing, Perth, Singapore, Hong Kong
+                        </option>
+                        <option value="9">
+                          (GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk
+                        </option>
+                        <option value="9.5">
+                          (GMT +9:30) Adelaide, Darwin
+                        </option>
+                        <option value="10">
+                          (GMT +10:00) Eastern Australia, Guam, Vladivostok
+                        </option>
+                        <option value="11">
+                          (GMT +11:00) Magadan, Solomon Islands, New Caledonia
+                        </option>
+                        <option value="12">
+                          (GMT +12:00) Auckland, Wellington, Fiji, Kamchatka
+                        </option>
+                      </select>
+                    )}
                   </div>
-                </div>
-
+                </div>{" "}
               </div>
             </form>
             <div className="form-group text-end">
@@ -279,5 +277,4 @@ function Schedule(props) {
     </div>
   );
 }
-
 export default Schedule;
--- src/components/ScheduleCall/slotGenerator.js
+++ src/components/ScheduleCall/slotGenerator.js
@@ -1,78 +1,153 @@
 function formatConverter(obj) {
+
     const timeObj = obj.split(' ');
+
     if (timeObj[1] === 'AM') {
+
         let tempTime = Number(timeObj[0].split(':')[0]);
+
         return + tempTime + (Math.floor(Number(timeObj[0].split(':')[1])) / 60)
+
     }
+
     let tempTime = Number(timeObj[0].split(':')[0]) + 12;
+
     return tempTime + (Math.floor(Number(timeObj[0].split(':')[1])) / 60);
+
 }
 
+ 
+
 function posssibleSlots(timeObj) {
+
     timeObj['time_difference'] = '0 min'
+
     let finalSlots = [];
+
     const {
+
         time_duration,
+
         start_time,
+
         end_time,
+
         time_difference
+
     } = timeObj;
+
     if (!(time_difference && start_time && end_time && time_difference && time_duration)) {
+
         return new Error('insufficient Information');
+
     }
+
+ 
 
     let toatalTime = formatConverter(end_time) - formatConverter(start_time);
 
+ 
+
     let divTime = (Number(time_difference.split(' ')[0]) + Number(time_duration.split(' ')[0]));
 
+ 
+
     let slots = (toatalTime * 60) / divTime;
+
     function genTime(last, diff) {
+
         let timeStr = '';
-        let endTime = '';
+
+ 
 
         let mins = Number(last.split(' ')[0].split(':')[1]);
+
         let slot = Number(time_duration.split(' ')[0]);
 
+ 
+
         if (diff !== undefined)
+
             slot += Number(time_difference.split(' ')[0]);
+
         let toatalMins = mins + slot;
+
         if (toatalMins >= 60) {
+
             let minStr = `${toatalMins - 60}`;
+
             let time1 = (Number(last.split(':')[0]) + 1) % 24;
+
             let timeNotation = undefined;
+
             if (time1 > 12) {
+
                 timeNotation = (time1 > 12) ? 'PM' : 'AM';
+
             } else {
-                if ((time1 == 1)) {
+
+                if ((time1 === 1)) {
+
                     timeNotation = (last.split(' ')[1] === 'AM') ? 'PM' : 'AM';
+
                 } else {
+
                     timeNotation = last.split(' ')[1];
+
                 }
+
             }
+
             time1 = time1 > 12 ? time1 % 12 : time1;
+
             time1 = (String(time1).length < 2) ? ('0' + time1) : time1;;
+
             timeStr = `${time1}:${minStr.length > 1 ? minStr : '0' + minStr} ${timeNotation}`
-            endTime = ``
+
+ 
+
         } else {
+
             let timeNotation = (last.split(' ')[1] !== undefined) ?
+
                 last.split(' ')[1]
+
                 : (Number(last.split(':')[0]) < 12) ? 'AM' : 'PM';
+
             timeStr = `${last.split(':')[0]}:${toatalMins} ${timeNotation}`
+
         }
+
+ 
 
         return timeStr;
-    }
-    for (let x = 0; x < Math.floor(slots); x++) {
-        const startTime = Number(start_time) <= 12 ? start_time + ' AM' : start_time + ' PM';
-        let start = x === 0 ? startTime : genTime(finalSlots[x - 1].starts, true)
-        let end = genTime(start, undefined);
-        finalSlots[x] = {
-            starts: start,
-            ends: end
-        }
+
     }
 
+    for (let x = 0; x < Math.floor(slots); x++) {
+
+        const startTime = Number(start_time) <= 12 ? start_time + ' AM' : start_time + ' PM';
+
+        let start = x === 0 ? startTime : genTime(finalSlots[x - 1].starts, true)
+
+        let end = genTime(start, undefined);
+
+        finalSlots[x] = {
+
+            starts: start,
+
+            ends: end
+
+        }
+
+    }
+
+ 
+
     return finalSlots;
+
 }
+
+ 
 
 export { posssibleSlots }
